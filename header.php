e8583c55673990e117d62045e43fc2ba9daf400e
fran8narf <srlk_doblev@hotmail.com>
2017-01-10 17:21:01 +0100

    First commit

---
+++ header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="ie ie8" lang="es"> <![endif]-->
+<!--[if IE 9 ]><html class="ie ie9" lang="es"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="es"> <!--<![endif]-->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>8belts</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	<meta name="distributor" content="Global" /> 
+	<link rel="stylesheet" href="css/mediaelementplayer.min.css" />
+	<link href="css/common.css" rel="stylesheet" type="text/css">
+	<link href="css/estilos.css" rel="stylesheet" type="text/css">
+	<link href="css/header.css" rel="stylesheet" type="text/css">
+	<link href="css/side-menu.css" rel="stylesheet" type="text/css">
+	<link href="css/popup_generico.css" rel="stylesheet" type="text/css">
+	<link href="css/pop-up.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+</head>
+<body>
+<header id="top">
+	<div class="wrapper">
+		<a class="logo" href="index.php">
+		</a>
+		<h5 class="globe_lng">
+			<a href="#">
+				<i class="globe_us_icon"></i>
+				<i class="arrow-drop"></i>
+			</a>
+			<span class="nav-sub-menu">
+				<a class="nav-sub-menu-item" href="#">
+					<i class="globe_us_icon"></i>
+					<span>EN</span>
+				</a>
+				<a class="nav-sub-menu-item" href="#">
+					<i class="globe_chino_icon"></i>
+					<span>CH</span>
+				</a>
+			</span>
+		</h5>
+		<div class="right_head_panel">
+			<ul class="admin_rht">
+				<li class="notification">
+					<a href="#">
+						<span class="bell_icon"><i class="notify-num">2</i></span>
+					</a>
+				</li>
+				<li class="profile-menu">
+					<a href="#">
+					<span class="usr_name">Yency</span>
+					<i class="arrow"></i>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>	
+</header>
---
+++ index.php
@@ -0,0 +1,76 @@
+<?php include("header.php"); ?>
+<div id="contenidoGeneral">
+	<div class="wrapper">
+		<div class="left_menu">
+			<nav class="left_nav">
+				<ul>
+					<li class="show">
+						<a href="#">
+							Dashboard
+							<i class="gauge-icon"></i>
+						</a>
+					</li>
+					<li class="active">
+						<a href="#">
+							<span class="top-part"><i class="small-chat-icon"></i> Expresiones</span>
+							<span class="repasos_num">
+								<i>12</i>
+								Repasos
+							</span>
+							<span class="nuevas_num">
+								<i>3</i>
+								Nuevas
+							</span>
+						</a>
+					</li>
+					<li class="show">
+						<a href="#">
+							<span class="top-part"><i class="small-play-icon"></i> Diálogos</span>
+							<span class="repasos_num">
+								<i>0</i>
+								Pendientes
+							</span>
+							<span class="nuevas_num">
+								<i>1</i>
+								Repasado
+							</span>
+						</a>
+					</li>
+					<li class="show">
+						<a href="#">
+							<span class="top-part"><i class="small-mic-icon"></i> Conversaciones</span>
+							<span class="repasos_num">
+								<i>0</i>
+								Hechas
+							</span>
+							<span class="nuevas_num">
+								<i>1</i>
+								Nueva
+							</span>
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+		<div class="right_wrapper primeras-section">
+			<ul class="tab">
+			  <li><a href="javascript:void(0)" class="tablinks" onclick="openTab(event, 'datos')">MIS DATOS</a></li>
+			  <li><a href="javascript:void(0)" class="tablinks" onclick="openTab(event, 'reto')">RETO DIARIO</a></li>
+			  <li><a href="javascript:void(0)" class="tablinks" onclick="openTab(event, 'estadisticas')">ESTADÍSTICAS</a></li>
+			  <li><a href="javascript:void(0)" class="tablinks" onclick="openTab(event, 'logros')">MIS LOGROS</a></li>
+			</ul>
+
+			<div id="datos" class="tabcontent"></div>
+			<div id="reto" class="tabcontent"></div>
+			<div id="estadisticas" class="tabcontent"></div>
+			<div id="logros" class="tabcontent"></div>
+
+		</div>	
+	</div><!--end wrapper-->
+</div>
+
+
+</body>
+</html>
+
+
